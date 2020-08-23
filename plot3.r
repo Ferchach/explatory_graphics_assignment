327415db52f5be57f35a422e9b7dfd4fe26e0965
Ferchach <64144197+Ferchach@users.noreply.github.com>
2020-08-23 19:44:47 +0100

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -15,7 +15,7 @@ lines(ELPC$time, ELPC$Sub_metering_1)
 lines(ELPC$time, ELPC$Sub_metering_2, col = 'red')
 lines(ELPC$time, ELPC$Sub_metering_3, col = 'blue')
 legend('topright', 
-	pch = '_' , 
+	lty = 1 , 
 	col = c('black', 'red', 'blue'), 
 	legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
 dev.off()
